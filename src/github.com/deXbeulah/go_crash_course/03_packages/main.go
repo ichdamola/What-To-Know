f10c524b798a81d29d2fbe564154c5cc0024f8fc
Adedamola Adedapo <adedadamola@gmail.com>
2020-04-17 22:17:11 +0100

    About "math" package.

---
+++ src/github.com/deXbeulah/go_crash_course/03_packages/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+"fmt"
+"math"
+// Path to package
+"github.com/deXbeulah/go_crash_course/03_package/strutil"
+)
+
+func main () {
+	fmt.Println(math.Floor(2.5))	
+	fmt.Println(math.Ceil(2.5))
+	fmt.Println(math.Sqrt(16))
+	fmt.Println(strutil.Reverse(olleh))
+	
+}
