0c321f5661eac95768c59e39d9983f6858c04c5c
Adedamola Adedapo <adedadamola@gmail.com>
2020-04-20 01:04:39 +0100

    Added the "getSum( )" function
    None.

--- src/github.com/deXbeulah/go_crash_course/04_function/main.go
+++ src/github.com/deXbeulah/go_crash_course/04_function/main.go
@@ -1,13 +1,19 @@
 package main
 
-import "fmt"
-
+import (
+"fmt"
+)
 
 func greetings(name string) string {
 	return "Hello " + name
 }
 
+func getSum(number1, number2 int) int{
+	return(number1 + number2)
+}
+
 func main () {
 	fmt.Println(greetings("Brad"))
+	fmt.Println(getSum(4,5))
 	
 }
