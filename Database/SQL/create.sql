2660ad24d015431aa677c37f0ea2305bbe410da8
Adedamola <adedadamola@gmail.com>
2020-07-18 21:30:55 +0100

    [IMP] Learn to use the SELECT commmand with DISTINCT to return non-duplicate values.

--- Database/SQL/create.sql
+++ Database/SQL/create.sql
@@ -2,3 +2,6 @@
 SELECT * FROM table_name;
 -- To select specific columns  from a table_name
 SELECT column_name_1, column_name_2 FROM table_name;
+
+-- To to select distinct values in a columns in a table in a db
+SELECT DISTINCT columns_name  FROM table_name;
