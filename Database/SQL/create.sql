fff0f610c73dba8eb9d33d30b20283b9aea89e00
Adedamola <adedadamola@gmail.com>
2020-07-18 21:23:21 +0100

    [IMP] Create use instances for the select command in SQL.

--- Database/SQL/create.sql
+++ Database/SQL/create.sql
@@ -1,1 +1,4 @@
-SELECT Name FROM sys.sysdatabases
+-- To to select all the fields in a Table named "table_name"
+SELECT * FROM table_name;
+-- To select specific columns  from a table_name
+SELECT column_name_1, column_name_2 FROM table_name;
