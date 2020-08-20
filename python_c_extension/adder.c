21231eebdf0d088316da130e0311798ec0920324
Adedamola <adedadamola@gmail.com>
2020-08-21 00:56:25 +0100

    Create the python C entension first file.

---
+++ python_c_extension/adder.c
@@ -0,0 +1,14 @@
+//sample C file to add two numbers - int and floats
+// gcc -shared -Wl,-soname,adder -o adder.so -fPIC add.c
+
+int add_int(int, int);
+float add_float(float, float);
+
+int add_int(int num1, int num2){
+	return num1 + num2;
+}
+
+float add_float(float num1, float num2){
+	return num1 + num2;
+}
+
