9228fd6cdfc4e6a51f0f7ff538a324d02c274b98
Adedapo Adedamola <adedadamola@gmail.com>
2021-06-23 16:25:55 +0100

    Added the data structures.

---
+++ Data-structures-and-algorithms/expandTheInputString.py
@@ -0,0 +1,21 @@
+# Phone interview question: expand the given input string in the following way
+
+# 3[a] = aaa
+# 3[a2[b]] = abbabbabb
+# 2[cd] = cdcd
+
+def stringExpander(stringValue):
+    num = 0
+    output = ''
+    leftBracket = 0
+    rightBracket = 0
+    i = 0
+    j = 0
+
+    while ()
+
+
+print (stringExpander('3[a]'))
+#aaa
+print (stringExpander('3[a2[b]]'))
+#abbabbabb
