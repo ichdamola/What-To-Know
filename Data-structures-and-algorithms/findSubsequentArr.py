c2a4cc76818a236f04ea3ef9ca02032d9bb7fb9d
Adedapo Adedamola <adedadamola@gmail.com>
2021-07-14 22:33:44 +0100

    Improved the linear time complexity of the script.

--- Data-structures-and-algorithms/findSubsequentArr.py
+++ Data-structures-and-algorithms/findSubsequentArr.py
@@ -1,14 +1,26 @@
-def subsequent_array_finder(arr1: list, arr2: list) -> bool:
-    index_of_arr1_element, index_of_arr2_element = 0,0
+# def subsequent_array_finder(arr1: list, arr2: list) -> bool:
+#     index_of_arr1_element, index_of_arr2_element = 0,0
 
-    for x in range(len(arr2)):
-        for y in range(len(arr1)):
-            if arr2[x] == arr1[y]:
-                index_of_arr1_element = index_of_arr2_element
-                index_of_arr2_element = y
-                if index_of_arr1_element > index_of_arr2_element:
-                    return False
-    return True
+#     for x in range(len(arr2)):
+#         for y in range(len(arr1)):
+#             if arr2[x] == arr1[y]:
+#                 index_of_arr1_element = index_of_arr2_element
+#                 index_of_arr2_element = y
+#                 if index_of_arr1_element > index_of_arr2_element:
+#                     return False
+#     return True
+
+def subsequent_array_finder(arr1: list, arr2: list) -> bool:
+    # O(n) time | O(1) space (n is the length of arr1)
+    arr2_pointer = 0
+
+    for i in range(len(arr1)):
+        if arr1[i] == arr2[arr2_pointer]:
+            arr2_pointer += 1
+    
+    arr2_is_fully_traversed = arr2_pointer == len(arr2)
+
+    return arr2_is_fully_traversed
 
 
 if __name__=="__main__":
