46f31c1dbfbc49fd236480691a2630b0270fe721
Adedapo Adedamola <adedadamola@gmail.com>
2021-07-20 19:58:07 +0100

    The script returns the most frequent element of an array.

---
+++ Data-structures-and-algorithms/mostFreqeentElement.py
@@ -0,0 +1,19 @@
+def most_frequent(arr):
+    elements = {}
+    max_count = 0
+    max_element = None
+
+    for element in arr:
+        if element not in elements:
+            elements[element] = 1
+        else:
+            elements[element] += 1
+
+        if elements[element] > max_count:
+            max_count = elements[element]
+            max_element = element
+
+    return max_element
+
+if __name__ == "__main__":
+    assert most_frequent([1,1,1,1,2,2,3,1,4,5]) == 1, "Test case failed!"
